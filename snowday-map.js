da382e38f947855e57b99ebdd33a3da2a7e3c3d5
edscamera <42257904+edscamera@users.noreply.github.com>
2024-11-29 13:58:03 -0500

    Simplified code. Fixed district names

--- snowday-map.js
+++ snowday-map.js
@@ -1,36 +1,36 @@
-//const src = "http://192.168.5.177/webpage.html";
 const src = "https://itv.news12.com/school_closings/closings.jsp?region=LI";
+//const src = `http://192.168.50.219:8080/School%20Closings%20and%20Delays.htm`;
 
+const simplify = (str) => str.replace(/\s|\/|-/g, "").toLowerCase();
 const toTitleCase = (str) =>
   str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
-const districts = {};
 
-window.addEventListener("load", () => {
+// Parse data
+const refreshData = (src) => {
   const srcParent = document.createElement("div");
   $(srcParent).load(src, () => {
     const rows = Array.from($($(srcParent).find("table")[1]).find("tbody").children());
     rows.forEach((row) => {
       const name = row.children[0].innerText.substr(4);
-      const status = row.children[2].innerText.substr(6);
-      if (/ufsd|school district/i.test(name)) {
-        const unformattedName = name.replace(/\s|\/|-/g, "").toLowerCase();
-        Object.keys(districts).forEach((district) => {
-          if (unformattedName.includes(district)) {
-            if (status.toLowerCase().includes("closed")) {
-              districts[district].style.fill = "red";
-            }
-            if (status.toLowerCase().includes("remote")) {
-              districts[district].style.fill = "darkred";
-            }
-            if (status.toLowerCase().includes("delay")) {
-              districts[district].style.fill = "yellow";
-            }
-          }
-        });
+      const status = simplify(row.children[2].innerText.substr(6));
+      if (/ufsd|schooldistrict|publicschool|centralsd|csd/i.test(simplify(name))) {
+        const district = Array.from(document.querySelectorAll("polygon"))
+          .find(x => simplify(name).startsWith(simplify(x.getAttribute("district"))));
+        if (district) {
+          if (status.includes("closed"))
+            district.style.fill = "red";
+          else if (status.includes("remote"))
+            district.style.fill = "darkred";
+          else if (status.includes("delay"))
+            district.style.fill = "yellow";
+        }
       }
     });
+    console.log(`[${new Date().toLocaleTimeString()}] Refreshed`);
   });
-});
+};
+window.addEventListener("load", () => refreshData(src));
+setInterval(() => refreshData(src), 60000);
 
 // Panning
 const svg = $("svg")[0];
@@ -75,5 +75,4 @@ document.querySelectorAll("polygon").forEach((district) => {
     $("#mouse")[0].innerText = toTitleCase(`${status.replace(/-/g, " ")}\n${getStatus(district.style.fill)}`);
     $("#mouse")[0].style.display = "block";
   });
-  districts[district.getAttribute("district").replace(/-/g, "")] = district;
 });
